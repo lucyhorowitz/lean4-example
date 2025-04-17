758e2691f907878634247369746a5c682b034f8b
Lucy Horowitz <111657652+lucyhorowitz@users.noreply.github.com>
2025-04-16 20:54:23 -0700

    move all to one file?

--- Lean4Example.lean
+++ Lean4Example.lean
@@ -1,3 +1,8 @@
+
+import Mathlib.Tactic
+-- imports all the Lean tactics
+import Mathlib.Order.Filter.Basic
+
 open Nat (add_assoc add_comm)
 
 theorem hello_world (a b c : Nat)
@@ -5,3 +10,32 @@ theorem hello_world (a b c : Nat)
   rw [add_assoc, add_comm b, ←add_assoc]
 
 theorem foo (a : Nat) : a + 1 = Nat.succ a := by rfl
+
+
+
+variable {α : Type}
+variable (p q r : Prop)
+
+open Filter Set
+-- so we don't keep having to type `Filter.le_def` and `Set.Subset.trans` etc
+
+open scoped Filter
+
+theorem test1 (S T : Set α) : 𝓟 S ≤ 𝓟 T ↔ S ⊆ T := by
+  constructor
+  · intro h
+    rw [le_def] at h
+    have hT : T ∈ 𝓟 T := mem_principal_self T
+    specialize h T hT
+    rwa [mem_principal] at h
+  · intro hST
+    rw [le_def]
+    intro X hX
+    rw [mem_principal] at hX ⊢
+    exact Subset.trans hST hX
+
+theorem test2 : (p ∧ q) → (p → q → r) → r := by
+  intro h1 h2
+  apply h2
+  exact h1.1
+  exact h1.2
